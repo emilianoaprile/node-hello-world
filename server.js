1a26b95a1d85bdbdde8e21d773bbfb00e19b3e10
EmilianoAprile <emilianoaprile77@icloud.com>
2024-05-21 15:05:52 +0200

    add > hello world variable

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const http = require("http");
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost"
+require('dotenv').config();
+const message = process.env.HELLO_WORLD;
 
 http
     .createServer((req, res) => {
         res.writeHead(200, {
             "Content-type" : "text/html"
         });
-        res.end("Hello-world")
+        res.end(`<h1>${message}</h1>`)
     })
     .listen(port, host, () => {
         const serverUrl = `http://${host}:${port}`
