c5dea4571f6b3bd7077a8e2b401eb96b1c0d37c2
EmilianoAprile <emilianoaprile77@icloud.com>
2024-05-21 15:00:29 +0200

    add > server config

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || "localhost"
+
+http
+    .createServer((req, res) => {
+        res.writeHead(200, {
+            "Content-type" : "text/html"
+        });
+        res.end("Hello-world")
+    })
+    .listen(port, host, () => {
+        const serverUrl = `http://${host}:${port}`
+        console.log(`Server avviato su ${serverUrl}`)
+    })
